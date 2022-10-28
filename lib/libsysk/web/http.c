824e1ef88bc6b9fbe790cd19a17e6dc18007f0e6
0kalekale <kalekale.anon@gmail.com>
2022-10-28 22:23:00 +0545

    libsysk: basic http_connect() and get() functions

--- lib/libsysk/web/http.c
+++ lib/libsysk/web/http.c
@@ -1,3 +1,21 @@
+/*
+    sysk/web/http: A simple http client server library.
+    Copyright (C) 2022 0kalekale
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,8 +26,11 @@
 
 #include <sysk/web/http.h>
 
-int get(uint16_t port, const char* addr) {
-	int sock, sockfd;
+int sock;
+int sockfd;
+
+int http_connect(uint16_t port, const char* addr) {
+
 	if (!(sock = socket(AF_INET, SOCK_STREAM, 0))) {
 		fprintf(stderr,  "[error] : could not create a socket");
 		return 1;
@@ -29,4 +50,25 @@ int get(uint16_t port, const char* addr) {
 		return 1;
 	}
 	return 0;
+
+}
+
+int get(char *message, void* resp_buffer) {
+
+	if(!message) {
+		fprintf(stderr, "[error] null pointer *message");
+		return 1;
+	}
+
+	if(!send(sock, message, strlen(message), 0)) {
+		fprintf(stderr, "[error] could not send message");
+		return 1;
+	}
+
+	// TODO: read bytes one by one and append to butter.
+	if(!read(sock, resp_buffer, sizeof(resp_buffer))) /* VERY bad way to do this */{
+		fprintf(stderr, "[error] could not read response into buffer");
+		return 1;
+	}
+
 }

37918485c9c4cb45695d75c02265141c21316327
0kalekale <kalekale.anon@gmail.com>
2022-11-11 21:05:10 +0545

    libkinet-meta: new project libkinet, depricates libsysk/web

---
+++ lib/libkinet/lib.rs
@@ -0,0 +1,4 @@
+#[no_mangle]
+pub extern "C" fn __this_is_a_place_holder() {
+	println!("placeholder");
+}
---
+++ lib/libkinet/test.c
@@ -0,0 +1,5 @@
+void __this_is_a_place_holder();
+
+int main() {
+	__this_is_a_place_holder();
+}

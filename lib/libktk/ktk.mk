35239cd1565273a45d3bc8589aa35ef61d680f70
0kalekale <kalekale.anon@gmail.com>
2022-11-21 12:22:33 +0545

    meta: replace temp build script with makefiles

--- lib/libktk/ktk.mk
+++ lib/libktk/ktk.mk
@@ -1,6 +1,6 @@
 libktk: 
-	gcc -c lib/libktk/ktk.c -Iinclude -o target/libktk.o
+	$(CC) -c lib/libktk/ktk.c -Iinclude -o target/libktk.o
 
 ktktest:
-	gcc -c lib/libktk/tests/sample.c -Iinclude -o target/tests/ktksample.o
-	gcc -o target/tests/ktksample target/tests/ktksample.o target/libktk.o -lX11 -lxcb
+	$(CC) -c lib/libktk/tests/sample.c -Iinclude -o target/tests/ktksample.o
+	$(CC) -o target/tests/ktksample target/tests/ktksample.o target/libktk.o -lX11 -lxcb

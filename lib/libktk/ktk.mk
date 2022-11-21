48a0e55b725d537df536f26a468d930911d1b068
0kalekale <kalekale.anon@gmail.com>
2022-11-21 11:30:38 +0545

    libktk: base off rxi's microui instead of writing from scratch with Xlib/xcb

--- lib/libktk/ktk.mk
+++ lib/libktk/ktk.mk
@@ -1,6 +1,6 @@
 libktk: 
 	gcc -c lib/libktk/ktk.c -Iinclude -o target/libktk.o
-
+	ar rcs target/libktk.a target/libktk.o 
 ktktest:
-	gcc -c lib/libktk/tests/sample.c -Iinclude -o target/tests/ktksample.o
-	gcc -o target/tests/ktksample target/tests/ktksample.o target/libktk.o -lX11 -lxcb
+	gcc lib/libktk/tests/main.c lib/libktk/tests/renderer.c target/libktk.a -Iinclude -Ilib/libktk/tests/ -o target/tests/ktksample `sdl2-config --libs` -lGL -lm
+

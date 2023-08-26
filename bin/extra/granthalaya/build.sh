8f222254a9169f06fbf8ae0a01314944455f54df
0kalekale <kalekale.anon@gmail.com>
2023-08-27 00:08:45 +0545

    refactor: oppsie fix symlinks

--- bin/extra/granthalaya/build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-rustc src/main.rs -o granthalaya/granthalaya -C inline-threshold=275 -C target-cpu=native
-strip granthalaya/granthalaya
--- bin/toolchain/lambdadelta/src/error.h
+++
@@ -1,4 +0,0 @@
-// FIXME: define this somewhere else
-extern int yylineno;     
-extern int rc; 
-void yyerror(char *s);

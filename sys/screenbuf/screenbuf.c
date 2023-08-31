0709877e225d73e8993ff66ae41e11428a7b5966
0kalekale <kalekale.anon@gmail.com>
2023-08-31 22:26:41 +0545

    sys/screenbuf: more work on screenbuf driver

--- sys/screenbuf/screenbuf.c
+++ sys/screenbuf/screenbuf.c
@@ -6,11 +6,10 @@
  * Compile with `make`. Load with `sudo insmod chardev.ko`. Check `dmesg | tail`
  * 
  * Under AGPL3
- */
-
-/*
- *	temp copied from https://gist.github.com/brenns10/65d1ee6bb8419f96d2ae693eb7a66cc0
- *	using it as a base of operation 
+ *
+ *
+ * note: temp copied from https://gist.github.com/brenns10/65d1ee6bb8419f96d2ae693eb7a66cc0
+ * now using it as a base of operation 
 */
 
 #include <linux/kernel.h>
@@ -29,12 +28,10 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
 #define SUCCESS 0
 #define DEVICE_NAME "screenbuf"
-#define BUF_LEN 80
 
-static int Major;
+static int major;
 static int Device_Open = 0;
-static char msg[BUF_LEN];
-static char *msg_Ptr;
+static char *pixbuf;
 
 static struct file_operations fops = {
   .read = device_read,
@@ -43,109 +40,72 @@ static struct file_operations fops = {
   .release = device_release
 };
 
-int init_module(void)
-{
-  Major = register_chrdev(0, DEVICE_NAME, &fops);
+int init_module(void) {
+  	major = register_chrdev(0, DEVICE_NAME, &fops);
 
-  if (Major < 0) {
-    printk(KERN_ALERT "Registering char device failed with %d\n", Major);
-    return Major;
-  }
+  	if (major < 0) {
+    		printk(KERN_ALERT "Registering char device failed with %d\n", Major);
+    		return major;
+ 	 }
 
-  printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
-  printk(KERN_INFO "the driver, create a dev file with\n");
-  printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
-  printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
-  printk(KERN_INFO "the device file.\n");
-  printk(KERN_INFO "Remove the device file and module when done.\n");
-
-  return SUCCESS;
+  	printk(KERN_INFO "I was assigned major number %d. To talk to\n", Major);
+  	printk(KERN_INFO "the driver, create a dev file with\n");
+  	printk(KERN_INFO "'mknod /dev/%s c %d 0'.\n", DEVICE_NAME, Major);
+  	printk(KERN_INFO "Try various minor numbers. Try to cat and echo to\n");
+  	printk(KERN_INFO "the device file.\n");
+  	printk(KERN_INFO "Remove the device file and module when done.\n");
+	
+	return SUCCESS;
 }
 
-void cleanup_module(void)
-{
-  unregister_chrdev(Major, DEVICE_NAME);
+void cleanup_module(void) {
+  	unregister_chrdev(major, DEVICE_NAME);
 }
 
-static int device_open(struct inode *inode, struct file *filp)
-{
-  static int counter = 0;
+static int device_open(struct inode *inode, struct file *filp) {
+  	static int counter = 0;
 
-  if (Device_Open)
-    return -EBUSY;
+  	if (Device_Open)
+    		return -EBUSY;
 
-  Device_Open++;
-  sprintf(msg, "I already told you %d times Hello world!\n", counter++);
-  msg_Ptr = msg;
-  /*
-   * TODO: comment out the line below to have some fun!
-   */
-  try_module_get(THIS_MODULE);
+  	Device_Open++;
+  	try_module_get(THIS_MODULE);
 
-  return SUCCESS;
+  	return SUCCESS;
 }
 
-static int device_release(struct inode *inode, struct file *filp)
-{
-  Device_Open--;
-
-  /*
-   * Decrement the usage count, or else once you opened the file, you'll never
-   * get rid of the module.
-   *
-   * TODO: comment out the line below to have some fun!
-   */
-  module_put(THIS_MODULE);
-
-  return SUCCESS;
+static int device_release(struct inode *inode, struct file *filp) {
+	Device_Open--;
+	module_put(THIS_MODULE);
+	return SUCCESS;
 }
 
 /*
- * Called when a process, which already opened the dev file, attempts to read
- * from it.
- */
-static ssize_t device_read(struct file *filp, /* see include/linux/fs.h   */
-                           char *buffer,      /* buffer to fill with data */
-                           size_t length,     /* length of the buffer     */
-                           loff_t *offset)
-{
-  /*
-   * Number of bytes actually written to the buffer
-   */
-  int bytes_read = 0;
+	get the size of the screen(how?)
+	alloc buffer			
+	write pixels to buffer(how??)
+	write buffer to user 
+*/
+static ssize_t device_read(struct file *filp, char *buffer, size_t length, off_t *offset) {	
+	int bytes_read = 0;
+	
+	/*
+		alloc pixbuf
+		and fill pixbuf
+	*/
+	
+	if (*pixbuf == 0)
+    		return 0;
 
-  /*
-   * If we're at the end of the message, return 0 signifying end of file.
-   */
-  if (*msg_Ptr == 0)
-    return 0;
-
-  /*
-   * Actually put the data into the buffer
-   */
-  while (length && *msg_Ptr) {
-    /*
-     * The buffer is in the user data segment, not the kernel segment so "*"
-     * assignment won't work. We have to use put_user which copies data from the
-     * kernel data segment to the user data segment.
-     */
-    put_user(*(msg_Ptr++), buffer++);
-    length--;
-    bytes_read++;
-  }
-
-  /*
-   * Most read functions return the number of bytes put into the buffer
-   */
-  return bytes_read;
+  	while (length && pixbuf) {
+ 		put_user(*(pixbuf++), buffer++);
+    		length--;
+    		bytes_read++;
+  	}
+	return bytes_read;
 }
 
-/*
- * Called when a process writes to dev file: echo "hi" > /dev/hello
- */
-static ssize_t
-device_write(struct file *filp, const char *buf, size_t len, loff_t *off)
-{
-  printk(KERN_ALERT "Sorry, this operation isn't supported.\n");
-  return -EINVAL;
+static ssize_t device_write(struct file *filp, const char *buf, size_t len, loff_t *off) {
+  	printk(KERN_ALERT "/dev/screenbuf: operation not allowed.\n");
+  	return -EINVAL;
 }

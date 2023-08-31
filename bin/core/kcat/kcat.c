0f143af488d37e48108cc606b46f529ae40f92f3
0kalekale <kalekale.anon@gmail.com>
2023-08-31 22:59:25 +0545

    bin/core/kcat.c: try not to write cancer challenge

--- bin/core/kcat/kcat.c
+++ bin/core/kcat/kcat.c
@@ -4,3 +4,38 @@ unix cat but with syntax highlighting for:
 - shell scripts 
 - assembly 
 */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <sysk/sysk.h>
+#include <sysk/vtesc.h>
+
+/*
+	TODO:void master(int argc, char *argv) {
+	get the build system working to link libsysk easily 
+*/
+
+
+// yes. 
+void c_highlight(char *word) {
+	if (strcmp(wordbuf, "int")) {
+		printf(GRN"int"RST);
+	}
+	else {	
+		printf("%s", wordbuf);
+	}
+}
+
+void asm_highlight(char *word) {
+	if (strcmp(wordbuf, "pop"))
+	// and so on.
+}
+int main (int argc,  char *argv) {		
+	
+	if (argc<2) {
+			// do something
+ 		}
+	}
+	
+} 

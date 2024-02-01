38f5cbfdce5ac3cdaf5b5615c36171f0feb90d22
0kalekale <kalekale.anon@gmail.com>
2024-02-01 14:39:39 +0545

    [lib/lbikmk]: experimental work on linalg

--- lib/libkmk/linalg.c
+++ lib/libkmk/linalg.c
@@ -1,3 +1,17 @@
 /*
 	sysk: linear algebra library (sped up with avx)
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <kmk/_simd.h>
+
+struct matrix {
+		double *elements; 
+		unsigned int x; 
+		unsigned int y;
+};
+
+void matrixadd(struct matrix *a, struct matrix *b, struct matrix *out) {
+	_simd_vecadd((v4df*)a->elements, (v4df*)b->elements, (v4df*)out->elements, a->x/a->y);
+} 
